e19b0c7db9206dd40d05ce70f687e74e3bdf3093
abner <1685578596@qq.com>
2021-12-22 14:32:26 +0800

    nacos动态刷新

--- cloudalibaba-config-nacos-client3377/src/main/java/com/abner/springcloud/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/abner/springcloud/controller/ConfigClientController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Abner
+ *  <dependency>
+ *    <groupId>com.alibaba.cloud</groupId>
+ *    <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ *  </dependency>
  * @RefreshScope Nacos的动态刷新功能。
  */
 @RestController
